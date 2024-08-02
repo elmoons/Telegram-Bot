26c2d3a59c6b89b41b37485dd8be227de7bdffe7
Mu4lka <shatnykhmax@mail.ru>
2024-08-02 20:41:29 +0700

    finalized

--- filters/is_subscribed.py
+++ filters/is_subscribed.py
@@ -8,9 +8,13 @@ from loader import bot
 
 class IsSubscribed(Filter):
     async def __call__(self, message: types.Message):
+        return await self.check(message.from_user.id)
+
+    @staticmethod
+    async def check(user_id) -> bool:
         chat_member = await bot.get_chat_member(
             chat_id=GROUP_CHAT_ID,
-            user_id=message.from_user.id
+            user_id=user_id
         )
         if (chat_member.status == ChatMemberStatus.LEFT or
                 chat_member.status == ChatMemberStatus.KICKED):
--- handlers/common.py
+++ handlers/common.py
@@ -1,4 +1,4 @@
-from aiogram import types, Router
+from aiogram import types, Router, F
 from aiogram.filters import Command
 from filters.is_private import IsChatPrivate
 from filters.is_subscribed import IsSubscribed
@@ -8,8 +8,17 @@ router = Router()
 
 
 @router.message(IsChatPrivate(), IsSubscribed())
-async def command_start(message: types.Message):
+async def send_web_app_link(message: types.Message):
     await message.answer(f"Откройте веб-приложения", reply_markup=get_web_app_inline_keyboard_markup())
+
+
+@router.callback_query(F.data == "Проверить подписку")
+async def handle_data_subscribed(callback_query: types.CallbackQuery):
+    if(await IsSubscribed().check(callback_query.from_user.id)):
+        await send_web_app_link(callback_query.message)
+    else:
+        await callback_query.message.delete()
+        await command_start(callback_query.message)
 
 
 @router.message(IsChatPrivate(), Command("start"))
@@ -17,4 +26,3 @@ async def command_start(message: types.Message):
     await message.answer(
         f"Для продолжения, вступите в группу и проверьте подписку",
         reply_markup=get_startup_inline_keyboard_markup())
-
