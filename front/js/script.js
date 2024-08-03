c3e07e41acd7982900116ee6f87e557b189ae3e1
Mu4lka <shatnykhmax@mail.ru>
2024-08-03 15:36:04 +0700

    minor changes

--- front/js/script.js
+++ front/js/script.js
@@ -3,13 +3,13 @@ document.getElementById("colorButton").addEventListener("click", () => {
   const numberOfSquaresToColor = Math.random() < 0.5 ? 4 : 5;
   const selectedIndices = [];
 
-  squares.forEach(square => square.classList.remove("colored"));
+  squares.forEach(square => square.classList.remove("starred"));
 
   while (selectedIndices.length < numberOfSquaresToColor) {
     const randomIndex = Math.floor(Math.random() * squares.length);
     if (!selectedIndices.includes(randomIndex)) {
       selectedIndices.push(randomIndex);
-      squares[randomIndex].classList.add("colored");
+      squares[randomIndex].classList.add("starred");
     }
   }
 });
