6455ec6d14cd54aeeb25f4179c046d32336acb9d
Mu4lka <shatnykhmax@mail.ru>
2024-08-03 12:50:52 +0700

    Изменил архитектуру фронта

--- front/js/script.js
+++ front/js/script.js
@@ -3,10 +3,8 @@ document.getElementById("colorButton").addEventListener("click", () => {
   const numberOfSquaresToColor = 5;
   const selectedIndices = [];
 
-  // Reset all squares
   squares.forEach(square => square.classList.remove("colored"));
 
-  // Select 5 random unique squares
   while (selectedIndices.length < numberOfSquaresToColor) {
     const randomIndex = Math.floor(Math.random() * squares.length);
     if (!selectedIndices.includes(randomIndex)) {
