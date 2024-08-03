8a101d1a83dcdfecdf7dd83b69c2fcb11f9ae5a6
Mu4lka <shatnykhmax@mail.ru>
2024-08-03 19:30:18 +0700

    updated

--- backend/tgbot/handlers/common.py
+++ backend/tgbot/handlers/common.py
@@ -8,14 +8,14 @@ router = Router()
 
 
 @router.message(IsChatPrivate(), IsSubscribed())
-async def send_web_app_link(message: types.Message):
+async def send_web_app(message: types.Message):
     await message.answer(f"Откройте веб-приложения", reply_markup=get_web_app_inline_keyboard_markup())
 
 
 @router.callback_query(F.data == "Проверить подписку")
 async def handle_data_subscribed(callback_query: types.CallbackQuery):
-    if(await IsSubscribed().check(callback_query.from_user.id)):
-        await send_web_app_link(callback_query.message)
+    if await IsSubscribed().check(callback_query.from_user.id):
+        await send_web_app(callback_query.message)
     else:
         await callback_query.message.delete()
         await command_start(callback_query.message)
