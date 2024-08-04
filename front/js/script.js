0299d3e252061349c9242385823c55ada5a59c0a
Mu4lka <shatnykhmax@mail.ru>
2024-08-04 17:31:19 +0700

    temp change

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.grid-container');
+    const numSquares = 25;
+
+    for (let i = 1; i < numSquares; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      container.appendChild(square);
+    }
+  });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const leftArrow = document.getElementById('left-arrow');
+    const rightArrow = document.getElementById('right-arrow');
+    const numberDisplay = document.getElementById('number-display');
+
+    const values = [1, 3, 5];
+    let index = 0;
+
+    leftArrow.addEventListener('click', () => {
+        if (index > 0) {
+            index--;
+            numberDisplay.textContent = values[index];
+        }
+    });
+
+    rightArrow.addEventListener('click', () => {
+        if (index < values.length - 1) {
+            index++;
+            numberDisplay.textContent = values[index];
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var header = document.getElementById('headerText');
+  var text = header.textContent;
+  var spans = text.split('').map(function(char, index) {
+    var delay = Math.random() * 2;
+    return `<span style="animation-delay: ${delay}s">${char}</span>`;
+    });
+  header.innerHTML = spans.join('');
+});
+
 document.getElementById("colorButton").addEventListener("click", () => {
   const squares = document.querySelectorAll(".square");
   const numberOfSquaresToColor = Math.random() < 0.5 ? 4 : 5;
@@ -17,14 +61,4 @@ document.getElementById("colorButton").addEventListener("click", () => {
       squares[randomIndex].classList.add("starred");
     }
   }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  var header = document.getElementById('headerText');
-  var text = header.textContent;
-  var spans = text.split('').map(function(char, index) {
-    var delay = Math.random() * 2;
-    return `<span style="animation-delay: ${delay}s">${char}</span>`;
-    });
-  header.innerHTML = spans.join('');
 });
