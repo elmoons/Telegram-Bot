0ed45feb6ee9bc62ff3a37104e58c473c88f64c8
Mu4lka <shatnykhmax@mail.ru>
2024-08-04 20:53:23 +0700

    temp change

--- front/js/script.js
+++ front/js/script.js
@@ -1,44 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.grid-container');
-    const numSquares = 25;
+  const container = document.querySelector('.grid-container');
+  const numSquares = 25;
+  const stars = [];
 
-    for (let i = 1; i < numSquares; i++) {
-      const square = document.createElement('div');
-      square.classList.add('square');
-      container.appendChild(square);
+  for (let i = 0; i < numSquares; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const leftArrow = document.getElementById('left-arrow');
+  const rightArrow = document.getElementById('right-arrow');
+  const numberDisplay = document.getElementById('number-display');
+
+  const values = [1, 3, 5];
+  let index = 0;
+
+  leftArrow.addEventListener('click', () => {
+    if (index > 0) {
+      index--;
+      numberDisplay.textContent = values[index];
     }
   });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const leftArrow = document.getElementById('left-arrow');
-    const rightArrow = document.getElementById('right-arrow');
-    const numberDisplay = document.getElementById('number-display');
-
-    const values = [1, 3, 5];
-    let index = 0;
-
-    leftArrow.addEventListener('click', () => {
-        if (index > 0) {
-            index--;
-            numberDisplay.textContent = values[index];
-        }
-    });
-
-    rightArrow.addEventListener('click', () => {
-        if (index < values.length - 1) {
-            index++;
-            numberDisplay.textContent = values[index];
-        }
-    });
+  rightArrow.addEventListener('click', () => {
+    if (index < values.length - 1) {
+      index++;
+      numberDisplay.textContent = values[index];
+    }
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var header = document.getElementById('headerText');
   var text = header.textContent;
-  var spans = text.split('').map(function(char, index) {
+  var spans = text.split('').map(function (char, index) {
     var delay = Math.random() * 2;
     return `<span style="animation-delay: ${delay}s">${char}</span>`;
-    });
+  });
   header.innerHTML = spans.join('');
 });
 
@@ -58,7 +59,11 @@ document.getElementById("starButton").addEventListener("click", () => {
     const randomIndex = Math.floor(Math.random() * squares.length);
     if (!selectedIndices.includes(randomIndex)) {
       selectedIndices.push(randomIndex);
-      squares[randomIndex].classList.add("starred");
     }
   }
+  selectedIndices.forEach((index, i) => {
+    setTimeout(() => {
+      squares[index].classList.add('starred');
+    }, i * 1000);
+  });
 });
